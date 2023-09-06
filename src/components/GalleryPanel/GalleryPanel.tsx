4006bb4ef63c1ff8e0392806fa998379fea9aea4
BrunoMorales28 <128781212+BrunoMorales28@users.noreply.github.com>
2023-09-05 22:26:25 -0300

    styled panel

--- src/components/GalleryPanel/GalleryPanel.tsx
+++ src/components/GalleryPanel/GalleryPanel.tsx
@@ -1,20 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, Theme, useMediaQuery } from "@mui/material";
 import SectionPanel from "./SectionPanel";
 import SortingPanel from "./SortingPanel";
 import ViralCheckbox from "./ViralCheckbox";
 import WindowPanel from "./WindowPanel";
 
-const GalleryPanel = () => (
-  <Box>
-    <Box my={3} display="flex" justifyContent="space-around">
-      <SectionPanel />
-      <ViralCheckbox />
+const GalleryPanel = () => {
+  const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.up("sm"));
+
+  return (
+    <Box>
+      <Box my={3} display="flex" justifyContent="space-around">
+        <SectionPanel />
+        <ViralCheckbox />
+      </Box>
+      <Box my={3} display="flex" justifyContent="center" flexDirection={isSmallScreen ? "row" : "column"} gap={3}>
+        <SortingPanel />
+        <WindowPanel />
+      </Box>
     </Box>
-    <Box my={3} display="flex" justifyContent="center" flexDirection="column">
-      <SortingPanel />
-      <WindowPanel />
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default GalleryPanel;
